64e23cc777582fb2ccc8a501d6102cb3dbd300ca
Natthanan Bhukan <natthanan.bhu@cjexpress.co.th>
2023-01-06 18:03:38 +0700

    add : update expense

--- app/src/services/expenses/get.go
+++ app/src/services/expenses/get.go
@@ -8,11 +8,11 @@ import (
 	"github.com/lib/pq"
 )
 
-func (h *handler) GetExpenseByID(echo echo.Context) error {
+func (h *handler) GetExpenseByID(c echo.Context) error {
 	var e Expenses
-	id := echo.Param("id")
+	id := c.Param("id")
 	if id == "" {
-		return echo.JSON(
+		return c.JSON(
 			http.StatusUnprocessableEntity,
 			ErrorResponse{Code: http.StatusUnprocessableEntity, Message: "Param id is empty"},
 		)
@@ -28,39 +28,39 @@ func (h *handler) GetExpenseByID(echo echo.Context) error {
 	if err != nil {
 		match, errMatch := regexp.MatchString("invalid input syntax", err.Error())
 		if match {
-			return echo.JSON(
+			return c.JSON(
 				http.StatusUnprocessableEntity,
 				ErrorResponse{Code: http.StatusUnprocessableEntity, Message: "Param id must be integer"},
 			)
 		}
 		if errMatch != nil {
-			return echo.JSON(
+			return c.JSON(
 				http.StatusInternalServerError,
 				ErrorResponse{Code: http.StatusInternalServerError, Message: err.Error()},
 			)
 		}
 		match, errMatch = regexp.MatchString("no rows in result set", err.Error())
 		if match {
-			return echo.JSON(
+			return c.JSON(
 				http.StatusNotFound,
 				ErrorResponse{Code: http.StatusNotFound, Message: "Record not found"},
 			)
 		}
 		if errMatch != nil {
-			return echo.JSON(
+			return c.JSON(
 				http.StatusInternalServerError,
 				ErrorResponse{Code: http.StatusInternalServerError, Message: err.Error()},
 			)
 		}
-		return echo.JSON(
+		return c.JSON(
 			http.StatusInternalServerError,
 			ErrorResponse{Code: http.StatusInternalServerError, Message: err.Error()},
 		)
 	}
-	return echo.JSON(http.StatusOK, e)
+	return c.JSON(http.StatusOK, e)
 }
 
-func (h *handler) GetExpenses(echo echo.Context) error {
+func (h *handler) GetExpenses(c echo.Context) error {
 	var expenses []Expenses
 
 	sql := `
@@ -69,7 +69,7 @@ func (h *handler) GetExpenses(echo echo.Context) error {
 	`
 	rows, err := h.db.Query(sql)
 	if err != nil {
-		return echo.JSON(
+		return c.JSON(
 			http.StatusInternalServerError,
 			ErrorResponse{Code: http.StatusInternalServerError, Message: err.Error()},
 		)
@@ -80,12 +80,12 @@ func (h *handler) GetExpenses(echo echo.Context) error {
 		var e Expenses
 		err := rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
 		if err != nil {
-			return echo.JSON(
+			return c.JSON(
 				http.StatusInternalServerError,
 				ErrorResponse{Code: http.StatusInternalServerError, Message: err.Error()},
 			)
 		}
 		expenses = append(expenses, e)
 	}
-	return echo.JSON(http.StatusOK, expenses)
+	return c.JSON(http.StatusOK, expenses)
 }
--- app/src/services/expenses/update.go
+++ app/src/services/expenses/update.go
@@ -1,1 +1,45 @@
 package expenses
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
+)
+
+func (h *handler) UpdateExpenseByID(c echo.Context) error {
+	exp := new(Expenses)
+	expenseId := c.Param("id")
+	if expenseId == "" {
+		return c.JSON(
+			http.StatusUnprocessableEntity,
+			ErrorResponse{Code: http.StatusUnprocessableEntity, Message: "Param id is empty"},
+		)
+	}
+
+	if err := c.Bind(exp); err != nil {
+		return c.JSON(
+			http.StatusUnprocessableEntity,
+			ErrorResponse{Code: http.StatusUnprocessableEntity, Message: "Invalid request body"},
+		)
+	}
+
+	sql := `
+	UPDATE 
+		expenses SET title = $1, amount = $2, note = $3, tags = $4
+	WHERE
+		id = $5
+	RETURNING id
+	`
+	row := h.db.QueryRow(sql, exp.Title, exp.Amount, exp.Note, pq.Array(&exp.Tags), expenseId)
+
+	err := row.Scan(&exp.ID)
+	if err != nil {
+		return c.JSON(
+			http.StatusInternalServerError,
+			ErrorResponse{Code: http.StatusInternalServerError, Message: err.Error()},
+		)
+	}
+
+	return c.JSON(http.StatusOK, exp)
+}
